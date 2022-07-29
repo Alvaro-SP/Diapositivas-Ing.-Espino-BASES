b5a30b35ae8ac459d44f3beeef07538e1d636aa9
Alvaro-SP <socop2412@gmail.com>
2022-07-28 21:25:42 -0600

    commit

--- a.py
+++ a.py
@@ -1,9 +1,15 @@
 import os
-file = open("1. Introduccion a bases de datos.md", "w")
-for i in range(24):
+file = open("4. Modelo relacional.md", "w")
+for i in range(117):
     if i<10:
-        file.write("![](http://kunusoft.com/slides/bd1/bd101_intro/Diapositiva0"+str(i)+".JPG)" + os.linesep)
+        file.write("![](http://kunusoft.com/slides/bd1/bd104_relacional/Diapositiva0"+str(i)+".JPG)" + os.linesep)
+    elif i>99:
+        if i<110:
+            file.write("![](http://kunusoft.com/slides/bd1/bd104_relacional/Diapositiva990"+str(i)+".JPG)" + os.linesep)
+        else:
+            file.write("![](http://kunusoft.com/slides/bd1/bd104_relacional/Diapositiva99"+str(i)+".JPG)" + os.linesep)
     else:
-        file.write("![](http://kunusoft.com/slides/bd1/bd101_intro/Diapositiva"+str(i)+".JPG)" + os.linesep)
+        file.write("![](http://kunusoft.com/slides/bd1/bd104_relacional/Diapositiva"+str(i)+".JPG)" + os.linesep)
+    
 
 file.close()
