d548c3d6990c78d06c00dc3afd5b46f950f451b3
Alvaro-SP <socop2412@gmail.com>
2022-08-03 09:16:17 -0600

    commit

--- a.py
+++ a.py
@@ -10,11 +10,11 @@ import os
 #             file.write("![](http://kunusoft.com/slides/bd1/bd104_relacional/Diapositiva99"+str(i)+".JPG)" + os.linesep)
 #     else:
 #         file.write("![](http://kunusoft.com/slides/bd1/bd104_relacional/Diapositiva"+str(i)+".JPG)" + os.linesep)
-file = open("6. Ejercicios en Data Modeler.md", "w")
-for i in range(29):
+file = open("766666. Ejercicios en Data Modeler.md", "w")
+for i in range(100):
     if i<10:
-        file.write("![](http://kunusoft.com/slides/bd1/bd106_dm/Diapositiva0"+str(i)+".JPG)" + os.linesep)
+        file.write("![](http://kunusoft.com/slides/bd1/bd102_modelos/Diapositiva0"+str(i)+".JPG)" + os.linesep)
     else:
-        file.write("![](http://kunusoft.com/slides/bd1/bd106_dm/Diapositiva"+str(i)+".JPG)" + os.linesep)
+        file.write("![](http://kunusoft.com/slides/bd1/bd102_modelos/Diapositiva"+str(i)+".JPG)" + os.linesep)
 
 file.close()
