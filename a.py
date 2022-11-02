c7bdc70f5dd2392df0e3061937d80619e1d287bb
Alvaro-SP <socop2412@gmail.com>
2022-11-01 22:06:33 -0600

    commit

--- a.py
+++ a.py
@@ -10,11 +10,11 @@ import os
 #             file.write("![](http://kunusoft.com/slides/bd1/bd104_relacional/Diapositiva99"+str(i)+".JPG)" + os.linesep)
 #     else:
 #         file.write("![](http://kunusoft.com/slides/bd1/bd104_relacional/Diapositiva"+str(i)+".JPG)" + os.linesep)
-file = open("766666. Ejercicios en Data Modeler.md", "w")
+file = open("8. Elementos complejos", "w")
 for i in range(100):
     if i<10:
-        file.write("![](http://kunusoft.com/slides/bd1/bd102_modelos/Diapositiva0"+str(i)+".JPG)" + os.linesep)
+        file.write("![](http://kunusoft.com/slides/bd1/bd108_complejos/Diapositiva0"+str(i)+".JPG)" + os.linesep)
     else:
-        file.write("![](http://kunusoft.com/slides/bd1/bd102_modelos/Diapositiva"+str(i)+".JPG)" + os.linesep)
+        file.write("![](http://kunusoft.com/slides/bd1/bd108_complejos/Diapositiva"+str(i)+".JPG)" + os.linesep)
 
 file.close()
